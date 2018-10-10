b7e6a59176cfec33cf8d713b2c81375f1f7d3a9e
spielchen <anna.bukavtsova@gmail.com>
2018-10-10 18:59:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 var money = prompt("Ваш бюджет на месяц?");
 var time = prompt("Введите дату в формате YYYY-MM-DD");
 
-var mainList = {
+var appData = {
 		money,
 		time, 
-		expenses: [],
-		income:{},
+		expenses: {},
+		income:[],
 		optionalExpenses:{},
-		savings: true
+		savings: false
 };
 
-console.log('Наш бюджет: '+mainList['money']);
-console.log('Дата: '+mainList['time']);
+console.log('Наш бюджет: ' + appData['money']);
+console.log('Дата: ' + appData['time']);
 
-function addNameGoods() {
+let expenses = {};
+		expenses.question ={};
+		a1 = prompt("Введите обязательную статью расходов в этом месяце");
+		a2 = prompt("Во сколько обойдется?");
+		a3 = prompt("Введите обязательную статью расходов в этом месяце");
+		a4 = prompt("Во сколько обойдется?");
+		expenses.question[a1] = a2;
+		expenses.question[a3] = a4;
 
-		for (var i = 0; i < 2; i++) {
-			mainList.expenses[i] = prompt("Введите обязательную статью расходов в этом месяце");
-	};
-		for (var a = 0; a < 2; a++){
-			mainList.expenses[i] = prompt("Во сколько обойдется?");
-		};
-		console.log(mainList.expenses[i] + ': ' + mainList.expenses[i]);
-};
-
-let expenses = addNameGoods();
-
+console.log(expenses);
 
 console.log('Бюджет за 1 день: '+Math.floor(money/30));
