1c065efdf735a7ca39ea421937070cc364080b0b
spielchen <anna.bukavtsova@gmail.com>
2019-04-27 02:06:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ var money = prompt("Ваш бюджет на месяц?");
 var time = prompt("Введите дату в формате YYYY-MM-DD");
 
 var appData = {
-		money,
-		time, 
+		budget,
+		timeData, 
 		expenses: {},
 		income:[],
 		optionalExpenses:{},
 		savings: false
 };
 
-console.log('Наш бюджет: ' + appData['money']);
-console.log('Дата: ' + appData['time']);
+var budget = console.log('Наш бюджет: ' + money);
+var timeData = console.log('Дата: ' + time);
 
-	appData.expenses = {};
+	//appData.expenses = {};
 
-		a1 = prompt("Введите обязательную статью расходов в этом месяце");
-		a2 = prompt("Во сколько обойдется?");
-		a3 = prompt("Введите обязательную статью расходов в этом месяце");
+	let	a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+		a2 = prompt("Во сколько обойдется?"),
+		a3 = prompt("Введите обязательную статью расходов в этом месяце"),
 		a4 = prompt("Во сколько обойдется?");
 		appData.expenses[a1] = a2;
 		appData.expenses[a3] = a4;
